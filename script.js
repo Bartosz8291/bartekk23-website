51b733e1ba191232c304c52e6bd919621bcbd1ac
Bartoszd (bartek3x) <164915721+Bartosz8291@users.noreply.github.com>
2025-03-28 15:40:53 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("nav ul li a");
+    
+    links.forEach(link => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const sectionId = this.getAttribute("href").substring(1);
+            const section = document.getElementById(sectionId);
+            if (section) {
+                window.scrollTo({
+                    top: section.offsetTop - 50,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
